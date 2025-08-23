df544f3b7487f7a4005e3bbd642afbfe3fa8cdfd
Kolby <kolbyheiss@gmail.com>
2025-08-22 22:19:46 -0700

    Added Menus and Map, added new Tamagotchi mockup!

--- game.gd
+++ game.gd
@@ -3,10 +3,44 @@ extends CanvasLayer
 @export var dogs : Array[DogResource] = []
 var temp 
 
+enum GameState {TITLE, DOGS, MENU, MAP}
+
+var state := GameState.MENU
+
 func _ready() -> void:
-	if !dogs:
-		%Dog.hide()
+	Globals.connect("change_state", change_state)
+
+func _process(delta: float) -> void:
+	match state:
+		GameState.TITLE:
+			%Title.show()
+		GameState.DOGS:
+			%Menu.hide()
+			%Map.hide()
+			%DogCollection.show()
+		GameState.MAP:
+			%Menu.hide()
+			%DogCollection.hide()
+			%Map.show()
+		GameState.MENU:
+			%Title.hide()
+			%DogCollection.hide()
+			%Map.hide()
+			%Menu.show()
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 	
+func change_state(new_state):
+	print("new state: " + str(new_state))
+	state = new_state
+
+func _on_menu_button_pressed() -> void:
+	print("changing State to Menu")
+	change_state(GameState.MENU)
+
+func _on_collection_button_pressed() -> void:
+	change_state(GameState.DOGS)
+	
+func _on_map_button_pressed() -> void:
+	change_state(GameState.MAP)
--- globals.gd
+++ globals.gd
@@ -1,4 +1,6 @@
 extends Node
 
+signal change_state(state)
+
 signal add_dog(DogResource)
 signal feed_dog
